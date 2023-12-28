b18d199d54ee367c38bf84a0f2bf002737041dc8
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-12-29 03:59:56 +0500

    move add_product to precondition in delete_test

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -4,16 +4,21 @@ from flower_shop_test.data.data import card, login_window
 from flower_shop_test.pages.base_page import BasePage
 
 
+@pytest.fixture()
+def pre_add_product():
+    main_page = BasePage()
+    main_page.add_product(card)
+
+
 def test_add_product(browser_managment):
     main_page = BasePage()
     main_page.open_url()
     main_page.add_product(card)
 
 
-def test_delete_product(browser_managment):
+def test_delete_product(browser_managment, pre_add_product):
     main_page = BasePage()
     main_page.open_url()
-    main_page.add_product(card)
     main_page.delete_product()
 
 
